85d34e29e3e6ce94faa514171a30c0a03e2a6d71
Vinay Vyas <vbvyas@gmail.com>
2014-05-27 22:15:26 -0700

    Refactoring

---
+++ app.js
@@ -0,0 +1,52 @@
+function draw() {
+  var canvas = document.getElementById('cv');
+  var context = canvas.getContext('2d');
+
+  // line
+  context.beginPath();
+  context.moveTo(100, 500);
+  context.lineTo(450, 50);
+  context.stroke();
+
+  // overlapping rectangles
+  if (canvas.getContext) {
+    context.fillStyle = "rgb(200, 0, 0)";
+    context.fillRect(10, 10, 55, 50);
+
+    context.fillStyle = "rgba(0, 0, 200, 0.5)";
+    context.fillRect(30, 30, 55, 50);
+  }
+
+  // circle
+  var centerX = canvas.width / 2;
+  var centerY = canvas.height / 2;
+  var radius = 70;
+
+  context.beginPath();
+  context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = 'teal';
+  context.fill();
+  context.lineWidth = 1;
+  context.strokeStyle = '#333333';
+  context.stroke();
+
+  // Bezier curve
+  context.beginPath();
+  // starting point
+  context.moveTo(200, 100);
+  // controlPoint1.{x, y}, controlPoint2.{x, y}, endingPoint.{x, y}
+  context.bezierCurveTo(150, 10, 400, 10, 400, 200);
+  context.lineWidth = 1;
+  context.strokeStyle = 'orange';
+  context.stroke();
+  
+  // Quadratic curve
+  context.beginPath();
+  // starting point
+  context.moveTo(300, 300);
+  // controlPoint.{x, y}, endingPoint.{x, y}
+  context.quadraticCurveTo(400, 0, 500, 200);
+  context.lineWidth = 1;
+  context.strokeStyle = '#123456';
+  context.stroke();
+}
